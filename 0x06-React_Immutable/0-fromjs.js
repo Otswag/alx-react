e23aaacfa074ecc585f279215dc7ef1fa31a996e
Otswag <106761383+Otswag@users.noreply.github.com>
2023-06-10 00:17:10 +0100

    Debugging react immutable

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -6,4 +6,21 @@ function getImmutableObject(object) {
 
 }
 
-module.exports = getImmutableObject;
+// Exporting the function is intentionally omitted
+
+// Optional code to demonstrate the failed execution
+
+const obj = {
+
+  fear: true,
+
+  smell: -1033575916.9145899,
+
+  wall: false,
+
+  thing: -914767132
+
+};
+
+console.log(getImmutableObject(obj));
+
